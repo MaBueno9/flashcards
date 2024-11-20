3683d30999fd0d7f48e27f386c890ad5e0ff4826
MaBueno9 <00001100908857sp@al.educacao.sp.gov.br>
2024-11-20 14:51:34 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Com quem eu me sento na sala?',
+    'Maisa'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem é a professora de geografia?',
+    'Claudinha'
+)
+
+criaCartao(
+    'Agro',
+    'Quanto custa uma cabeça de gado?',
+    ' R$ 4 mil, em média'
+)
+
+criaCartao(
+    'Agro',
+    'Quanto custa um hectare?',
+    '$ 14,3 mil na média nacional, mas varia até 26 mil vezes dependendo da região'
+)
